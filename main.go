0e2468c2d8ae44284e4380ff17eb8aa09fb4d0e2
Oleksii Velychko <oleksiivelychko@icloud.com>
2021-12-01 14:42:21 +0200

    setup entry point

---
+++ main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"database/sql"
+	_ "github.com/jinzhu/gorm/dialects/postgres"
+	"github.com/oleksiivelychko/go-account/handlers"
+	"github.com/oleksiivelychko/go-account/initdb"
+	"log"
+	"net/http"
+	"os"
+)
+
+func main() {
+	// initdb.LoadEnv() // uncomment to local development
+
+	db, err := initdb.DB()
+	if err != nil {
+		log.Fatalf("failed database connection: %s", err)
+	}
+
+	dbConnection, err := db.DB()
+	defer func(sqlDB *sql.DB) {
+		err = sqlDB.Close()
+		if err != nil {
+			log.Fatalf("unbale to close database connection: %s", err)
+		}
+	}(dbConnection)
+
+	http.HandleFunc("/api/account/register", handlers.RegisterHandler(db))
+	log.Fatal(http.ListenAndServe(":"+os.Getenv("PORT"), nil))
+}
