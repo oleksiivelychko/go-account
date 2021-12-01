ce6fcacc5ccef36f408aa0f4522f60494f1261c1
Oleksii Velychko <oleksiivelychko@icloud.com>
2021-12-01 08:23:28 +0200

    create register handler

---
+++ handlers/register_handler.go
@@ -0,0 +1,46 @@
+package handlers
+
+import (
+	"encoding/json"
+	"github.com/oleksiivelychko/go-account/models"
+	"gorm.io/gorm"
+	"net/http"
+)
+
+func RegisterHandler(db *gorm.DB) func(w http.ResponseWriter, r *http.Request) {
+	return func(w http.ResponseWriter, r *http.Request) {
+		if r.Method != "POST" {
+			return
+		}
+
+		w.Header().Set("Content-Type", "application/json")
+
+		var inputAccount models.Account
+		err := json.NewDecoder(r.Body).Decode(&inputAccount)
+		if err != nil {
+			w.WriteHeader(http.StatusBadRequest)
+			return
+		}
+
+		roleRepository := models.RoleRepository{DB: db, Debug: true}
+		var roles []models.Role
+		role, err := roleRepository.FindOneByNameOrCreate("user")
+		roles = append(roles, *role)
+
+		accountRepository := models.AccountRepository{DB: db, Debug: true}
+		account, err := accountRepository.Create(&models.Account{
+			Email:    inputAccount.Email,
+			Password: inputAccount.Password,
+			Roles:    roles,
+		})
+
+		if err != nil {
+			w.WriteHeader(http.StatusBadRequest)
+			_, _ = w.Write([]byte(err.Error()))
+			return
+		}
+
+		w.WriteHeader(http.StatusCreated)
+		_ = json.NewEncoder(w).Encode(account)
+	}
+}
