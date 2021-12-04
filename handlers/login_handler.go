e0ecca380e90c0c4876c934a06bf0cc3e14e3a2c
Oleksii Velychko <oleksiivelychko@icloud.com>
2021-12-04 06:12:25 +0200

    retrieve access token after successful login

--- handlers/login_handler.go
+++ handlers/login_handler.go
@@ -5,6 +5,8 @@ import (
 	"github.com/oleksiivelychko/go-account/models"
 	"gorm.io/gorm"
 	"net/http"
+	"os"
+	"strconv"
 )
 
 func LoginHandler(db *gorm.DB) func(w http.ResponseWriter, r *http.Request) {
@@ -28,6 +30,28 @@ func LoginHandler(db *gorm.DB) func(w http.ResponseWriter, r *http.Request) {
 		if err != nil {
 			w.WriteHeader(http.StatusUnauthorized)
 			_, _ = w.Write([]byte(err.Error()))
+			return
+		}
+
+		apiAccessTokenUrl := os.Getenv("API_ACCESS_TOKEN_URL")
+		if apiAccessTokenUrl == "" {
+			w.WriteHeader(http.StatusBadRequest)
+			_, _ = w.Write([]byte("API_ACCESS_TOKEN_URL is empty"))
+			return
+		}
+
+		var apiRequestUrl = apiAccessTokenUrl + "?userId=" + strconv.Itoa(int(account.ID))
+		response, err := http.Get(apiRequestUrl)
+		if err != nil {
+			w.WriteHeader(http.StatusBadRequest)
+			_, _ = w.Write([]byte("unable to make request to `" + apiRequestUrl))
+			return
+		}
+
+		err = json.NewDecoder(response.Body).Decode(&account)
+		if err != nil {
+			w.WriteHeader(http.StatusBadRequest)
+			_, _ = w.Write([]byte("unable to parse response body"))
 			return
 		}
 
