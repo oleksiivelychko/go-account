551013b21efd5c9b7d9daab29de9eefdff810d3a
Oleksii Velychko <oleksiivelychko@icloud.com>
2022-09-17 09:29:43 +0300

    replace dash with underscore in names for all json attributes

--- models/account.go
+++ models/account.go
@@ -27,11 +27,11 @@ type AccountSerialized struct {
 	ID             uint             `json:"id,omitempty"`
 	Email          string           `json:"email,omitempty"`
 	Roles          []RoleSerialized `json:"roles,omitempty"`
-	AccessToken    string           `json:"access-token,omitempty"`
-	RefreshToken   string           `json:"refresh-token,omitempty"`
-	ExpirationTime string           `json:"expiration-time,omitempty"`
-	ErrorMessage   string           `json:"error-message,omitempty"`
-	ErrorCode      uint8            `json:"error-code,omitempty"`
+	AccessToken    string           `json:"access_token,omitempty"`
+	RefreshToken   string           `json:"refresh_token,omitempty"`
+	ExpirationTime string           `json:"expiration_time,omitempty"`
+	ErrorMessage   string           `json:"error_message,omitempty"`
+	ErrorCode      int              `json:"error_code,omitempty"`
 }
 
 func (model *Account) VerifyPassword(password string) error {
