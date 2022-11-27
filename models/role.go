54151c22f736f66227c79a794a71b7f4d94a8e91
Oleksii Velychko <oleksiivelychko@icloud.com>
2022-11-27 08:58:43 +0200

    use compatible with json datetime type from utils package

--- models/role.go
+++ models/role.go
@@ -1,11 +1,13 @@
 package models
 
+import "github.com/oleksiivelychko/go-microservice/utils"
+
 type Role struct {
-	ID        uint      `gorm:"primary_key;auto_increment" json:"id"`
-	Name      string    `gorm:"size:50;not null;unique" json:"name"`
-	CreatedAt JSONTime  `gorm:"default:CURRENT_TIMESTAMP" json:"created_at"`
-	UpdatedAt JSONTime  `gorm:"default:CURRENT_TIMESTAMP" json:"updated_at"`
-	Accounts  []Account `gorm:"many2many:accounts_roles;constraint:OnUpdate:CASCADE,OnDelete:CASCADE;" json:"accounts,omitempty"`
+	ID         uint           `gorm:"primary_key;auto_increment" json:"id"`
+	Name       string         `gorm:"size:50;not null;unique" json:"name"`
+	CCreatedAt utils.DateTime `gorm:"-" json:"created_at"`
+	UpdatedAt  utils.DateTime `gorm:"-" json:"updated_at"`
+	Accounts   []Account      `gorm:"many2many:accounts_roles;constraint:OnUpdate:CASCADE,OnDelete:CASCADE;" json:"accounts,omitempty"`
 }
 
 type RoleSerialized struct {
