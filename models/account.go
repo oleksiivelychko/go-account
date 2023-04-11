9be5052f92dd5c3af94b8e86ca1bd80ecdf2f79b
Oleksii Velychko <oleksiivelychko@icloud.com>
2023-04-11 10:34:28 +0300

    cast to camelCase json fields, rename datetime

--- models/account.go
+++ models/account.go
@@ -5,21 +5,20 @@ import (
 )
 
 type Account struct {
-	ID        uint               `gorm:"primary_key;auto_increment" json:"id"`
-	Email     string             `gorm:"size:100;not null;unique" json:"email"`
-	Password  string             `gorm:"size:100;not null;" json:"password"`
-	CreatedAt utils.GormDateTime `json:"created_at"`
-	UpdatedAt utils.GormDateTime `json:"updated_at"`
-	Roles     []Role             `gorm:"many2many:accounts_roles;constraint:OnUpdate:CASCADE,OnDelete:CASCADE;"`
+	ID        uint           `gorm:"primary_key;auto_increment" json:"id"`
+	Email     string         `gorm:"size:100;not null;unique" json:"email"`
+	Password  string         `gorm:"size:100;not null;" json:"password"`
+	CreatedAt utils.DateTime `json:"createdAt"`
+	UpdatedAt utils.DateTime `json:"updatedAt"`
+	Roles     []Role         `gorm:"many2many:accounts_roles;constraint:OnUpdate:CASCADE,OnDelete:CASCADE;"`
 }
 
 type AccountSerialized struct {
 	ID             uint             `json:"id,omitempty"`
 	Email          string           `json:"email,omitempty"`
 	Roles          []RoleSerialized `json:"roles,omitempty"`
-	AccessToken    string           `json:"access_token,omitempty"`
-	RefreshToken   string           `json:"refresh_token,omitempty"`
-	ExpirationTime string           `json:"expiration_time,omitempty"`
-	ErrorMessage   string           `json:"error_message,omitempty"`
-	ErrorCode      uint8            `json:"error_code,omitempty"`
+	AccessToken    string           `json:"accessToken,omitempty"`
+	RefreshToken   string           `json:"refreshToken,omitempty"`
+	ExpirationTime string           `json:"expirationTime,omitempty"`
+	ErrorMessage   string           `json:"errorMessage,omitempty"`
 }
