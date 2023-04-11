d4ef9939e4e5bd4461cf524d57bd0dbeb18ec828
Oleksii Velychko <oleksiivelychko@icloud.com>
2023-04-11 10:34:37 +0300

    cast to camelCase json fields, rename datetime

--- models/role.go
+++ models/role.go
@@ -5,11 +5,11 @@ import (
 )
 
 type Role struct {
-	ID        uint               `gorm:"primary_key;auto_increment" json:"id"`
-	Name      string             `gorm:"size:50;not null;unique" json:"name"`
-	CreatedAt utils.GormDateTime `json:"created_at"`
-	UpdatedAt utils.GormDateTime `json:"updated_at"`
-	Accounts  []Account          `gorm:"many2many:accounts_roles;constraint:OnUpdate:CASCADE,OnDelete:CASCADE;" json:"accounts,omitempty"`
+	ID        uint           `gorm:"primary_key;auto_increment" json:"id"`
+	Name      string         `gorm:"size:50;not null;unique" json:"name"`
+	CreatedAt utils.DateTime `json:"createdAt"`
+	UpdatedAt utils.DateTime `json:"updatedAt"`
+	Accounts  []Account      `gorm:"many2many:accounts_roles;constraint:OnUpdate:CASCADE,OnDelete:CASCADE;" json:"accounts,omitempty"`
 }
 
 type RoleSerialized struct {
