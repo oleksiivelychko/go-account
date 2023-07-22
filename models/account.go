2f62b6b2213e7a6e6680a9f7017e8b762b87ed13
Oleksii Velychko <oleksiivelychko@icloud.com>
2023-07-22 14:05:11 +0300

    rename package

--- models/account.go
+++ models/account.go
@@ -1,15 +1,13 @@
 package models
 
-import (
-	"github.com/oleksiivelychko/go-account/utils"
-)
+import "github.com/oleksiivelychko/go-account/types"
 
 type Account struct {
 	ID        uint           `gorm:"primary_key;auto_increment" json:"id"`
 	Email     string         `gorm:"size:100;not null;unique" json:"email"`
 	Password  string         `gorm:"size:100;not null;" json:"password"`
-	CreatedAt utils.DateTime `json:"createdAt"`
-	UpdatedAt utils.DateTime `json:"updatedAt"`
+	CreatedAt types.DateTime `json:"createdAt"`
+	UpdatedAt types.DateTime `json:"updatedAt"`
 	Roles     []Role         `gorm:"many2many:accounts_roles;constraint:OnUpdate:CASCADE,OnDelete:CASCADE;"`
 }
 
--- models/role.go
+++ models/role.go
@@ -1,14 +1,12 @@
 package models
 
-import (
-	"github.com/oleksiivelychko/go-account/utils"
-)
+import "github.com/oleksiivelychko/go-account/types"
 
 type Role struct {
 	ID        uint           `gorm:"primary_key;auto_increment" json:"id"`
 	Name      string         `gorm:"size:50;not null;unique" json:"name"`
-	CreatedAt utils.DateTime `json:"createdAt"`
-	UpdatedAt utils.DateTime `json:"updatedAt"`
+	CreatedAt types.DateTime `json:"createdAt"`
+	UpdatedAt types.DateTime `json:"updatedAt"`
 	Accounts  []Account      `gorm:"many2many:accounts_roles;constraint:OnUpdate:CASCADE,OnDelete:CASCADE;" json:"accounts,omitempty"`
 }
 
